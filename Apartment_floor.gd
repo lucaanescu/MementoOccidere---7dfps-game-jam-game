79711310eafd93006f075cc9362039890aadd88f
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2024-12-07 16:42:31 -0600

    Player update
    player can move but not well yet, ill add the gun in later

---
+++ Apartment_floor.gd
@@ -0,0 +1,11 @@
+extends CSGBox3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _awake():
+	self.set_collison(true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
