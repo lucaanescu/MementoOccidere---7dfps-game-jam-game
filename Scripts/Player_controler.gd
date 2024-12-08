1942aa82fe3ab14bd8f3cd139e1ace7fde01b76f
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2024-12-08 02:17:17 -0600

    more controls changes
    locked the mouse in place and added a sprint

--- Scripts/Player_controler.gd
+++ Scripts/Player_controler.gd
@@ -1,21 +1,26 @@
 extends CharacterBody3D
 
-
-const SPEED = 2.0
+#Base values of the game that will change around
+var SPEED = 2.0
+var Sprinting = 4.0
 const JUMP_VELOCITY = 4.5
+const mousne_sense = 0.4
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_pressed("Sprint"):
+		SPEED = Sprinting
+	else:
+		SPEED = 2.0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
