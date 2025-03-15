353dfefd44248b322ca14d54e4ed98c0b8f7f39e
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2025-03-15 12:56:39 -0400

    Scene 1.5
    Added more changes to enviroment and cut vertecies in the main scene on the tents

--- Scripts/Text.gd
+++ Scripts/Text.gd
@@ -1,6 +1,6 @@
 extends Node
 
-#signal to display the dialog
+#signal to display the dialog from the display_dialog function in Interaction_object script
 signal display_dialog(text_key)
 
 func _on_text_interact(key):
--- Sounds/gun_pickup_script.gd
+++ Sounds/gun_pickup_script.gd
@@ -3,18 +3,10 @@ var Gun_body : StaticBody3D
 
 @export var dialog_key : String
 var area_active = false
-
-
-func _ready():
-	pass 
-
-func _process(delta):
-	pass
 	
-
 func _on_static_body_3d_mouse_entered() -> void:
 	area_active = true
-
-
+	$SpotLight3D.visible = false
+	
 func _on_static_body_3d_mouse_exited() -> void:
 	area_active = false
